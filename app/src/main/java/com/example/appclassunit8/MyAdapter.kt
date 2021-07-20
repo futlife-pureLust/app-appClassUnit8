2dcddaae13d879f0e2733a08351448475f6a24b6
futlife-pureLust <futlife.love.daimon@gmail.com>
2021-07-20 14:31:41 +0800

    v1

---
+++ app/src/main/java/com/example/appclassunit8/MyAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.appclassunit8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.adapter_row)
+    }
+}
